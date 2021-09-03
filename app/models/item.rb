0d5d65d4ceb024b280b75f451d44de45908630af
37Gin <yujiterao4425@outlook.jp>
2021-09-03 18:38:15 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery_fee
+  belongs_to :region
+  belongs_to :shipping_date
+
   belongs_to :user
   has_one_attached :image
+  
 end

9eef0a282b60cf0a21fdc22be7465f1603906a85
37Gin <yujiterao4425@outlook.jp>
2021-09-04 15:43:54 +0900

    商品登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,20 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item)
+    .permit( :title, :explanation, :category_id, :state_id, :delivery_fee_id, :region_id, :shipping_date_id, :price )
+    .merge(user_id: current_user.id)
   end
 
 end

0a94fbe99cc32429a9dd1392b7815b16b0fc5de3
37Gin <yujiterao4425@outlook.jp>
2021-09-04 14:37:49 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,16 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   
+  validates :title,            presence: true
+  validates :explanation,      presence: true
+  validates :category_id,      presence: true, numericality: { other_than: 1 }
+  validates :state_id,         presence: true, numericality: { other_than: 1 }
+  validates :delivery_fee_id,  presence: true, numericality: { other_than: 1 }
+  validates :region_id,        presence: true, numericality: { other_than: 1 }
+  validates :shipping_date_id, presence: true, numericality: { other_than: 1 }
+  validates :price,
+    presence: true,
+    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
+    format: { with: /\A[0-9]+\z/ }
+
 end

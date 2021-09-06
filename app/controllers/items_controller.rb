727dab89bb3a18b7588848f9946eead1bcc17004
37Gin <yujiterao4425@outlook.jp>
2021-09-06 20:49:43 +0900

    ユーザー認証機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: :edit
+
   def index
     @items = Item.all.includes(:user)
   end
@@ -40,5 +43,12 @@ class ItemsController < ApplicationController
     params.require(:item)
           .permit(:title, :explanation, :category_id, :state_id, :delivery_fee_id, :region_id, :shipping_date_id, :price, :image)
           .merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    unless current_user.id == item.user_id
+      redirect_to action: :index
+    end
   end
 end

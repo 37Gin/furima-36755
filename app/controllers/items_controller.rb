b6f1406ced285cbb6e6ba39bb7ca03cbf70e316d
37Gin <yujiterao4425@outlook.jp>
2021-09-06 20:32:46 +0900

    商品情報更新機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,13 @@ class ItemsController < ApplicationController
   end
 
   def update
+    item =  Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path(item.id)
+    else
+      @item = item
+      render :edit
+    end
   end
 
   private

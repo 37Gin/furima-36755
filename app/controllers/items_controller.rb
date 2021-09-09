b21d2e40f68fa51a57ac1155f349da11dccabac5
37Gin <yujiterao4425@outlook.jp>
2021-09-09 12:25:38 +0900

    売却済み確認とユーザの認証の設定修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :check_stock, except: [:index, :new, :show]
+  before_action :check_stock, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.includes(:user)
